6b4ae2903db0b847b77e05d255a48e2d59bd80ce
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2020-11-02 13:30:02 -0500

    Final Project Created    * This is a Movie Tracer Program

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main  extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root,Const.SCREEN_WIDTH,Const.SCREEN_HEIGHT);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
