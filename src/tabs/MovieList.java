d7ca9c1e15494b052dc97fc6e4732870b32c686b
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2020-11-04 10:46:54 -0500

    WatchedList Tab Created

--- src/tabs/MovieList.java
+++ src/tabs/MovieList.java
@@ -5,6 +5,6 @@ import javafx.scene.control.Tab;
 public class MovieList extends Tab {
     public MovieList() {
         super("Movie List");
-        System.out.print("Login Tab Wishlist Tab Watched List Tab Users Manage Tab");
+        
     }
 }
