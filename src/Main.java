01fd0d7c2a22619d06b47db8c3e96fac2144e05c
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2020-11-04 11:03:00 -0500

    #1 All Tabs Created    * All required tabs created    * All tabs added to TabPane which exdened JavaFX TabPane    * Instance of TabPane setted to center of BorderPane

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Panes.TabPane;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -8,6 +9,8 @@ public class Main  extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane root = new BorderPane();
+
+        root.setCenter(new TabPane());
         Scene scene = new Scene(root,Const.SCREEN_WIDTH,Const.SCREEN_HEIGHT);
 
         stage.setScene(scene);
