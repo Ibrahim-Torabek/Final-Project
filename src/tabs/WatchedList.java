f3de37875e22837933e9462bde2c9cb9225b9f21
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2020-11-12 00:05:31 -0500

    #18 Singletonilize Completed    * Changed all tabs to singleton    * Changed TabPane to singleton

--- src/tabs/WatchedList.java
+++ src/tabs/WatchedList.java
@@ -3,7 +3,17 @@ package tabs;
 import javafx.scene.control.Tab;
 
 public class WatchedList extends Tab {
-    public WatchedList() {
+    private static WatchedList tab;
+
+    private WatchedList() {
         super("Watched List");
+    }
+
+    public static WatchedList getInstance(){
+        if(tab == null){
+            tab = new WatchedList();
+        }
+
+        return tab;
     }
 }
