8b9240c562c4e059c883659a80f5647f739ed07d
Elena Polyakova <elena.polyakova98@stclairconnect.ca>
2020-11-12 20:23:57 -0500

    LogIntoDatabase.java added in .gitignore

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,12 @@ public class Main  extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
+
         BorderPane root = new BorderPane();
 
-        root.setCenter(TabPane.getInstance());
+        //root.setCenter(TabPane.getInstance());
         Scene scene = new Scene(root,Const.SCREEN_WIDTH,Const.SCREEN_HEIGHT);
-
+        stage.setTitle("Movie Tracker");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/database/LogIntoDatabase.java
@@ -0,0 +1,10 @@
+package database;
+
+public class LogIntoDatabase {
+
+    public static final String DB_NAME = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASSWORD = "";
+
+
+}
