f4cdb81d643922ccbd7dae8ed702d864ba969c85
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2020-11-25 02:01:39 -0500

    #14 CompanyListTab Completed    * Created CompanyList class    * Created CompanyListDAO interface    * Created CompanyListTable class    * Displayed Companies on CompanyList tab successfully

--- src/tabs/CompanyList.java
+++ src/tabs/CompanyList.java
@@ -1,12 +1,41 @@
 package tabs;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import tables.CompanyListTable;
 
 public class CompanyList extends Tab {
     private static CompanyList tab;
+    public TableView tableView;
 
     private CompanyList() {
         super("Movie Companies List");
+
+
+        CompanyListTable companyListTable = new CompanyListTable();
+        BorderPane root = new BorderPane();
+
+        tableView = new TableView();
+
+        TableColumn<pojo.CompanyList, String> columnId = new TableColumn<>("Company ID");
+        columnId.setCellValueFactory(
+                e -> new SimpleStringProperty(String.valueOf(e.getValue().getCompanyId()))
+        );
+
+        TableColumn<pojo.CompanyList, String> columnCompanyName = new TableColumn<>("Company Name");
+        columnCompanyName.setCellValueFactory(
+                e -> new SimpleStringProperty(e.getValue().getCompanyName())
+        );
+
+        tableView.getColumns().addAll(columnId,columnCompanyName);
+        tableView.getItems().addAll(companyListTable.getAllCompanies());
+        root.setCenter(tableView);
+
+        this.setContent(root);
+
     }
 
     public static CompanyList getInstance(){
--- src/tabs/MovieList.java
+++ src/tabs/MovieList.java
@@ -1,12 +1,25 @@
 package tabs;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import pojo.CompanyList;
+import tables.CompanyListTable;
 
 public class MovieList extends Tab {
     private static MovieList tab;
 
+
     private MovieList() {
         super("Movie List");
+
     }
 
     public static MovieList getInstance(){
@@ -16,4 +29,5 @@ public class MovieList extends Tab {
 
         return tab;
     }
+
 }
