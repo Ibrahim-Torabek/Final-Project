a082d326a7a8000a8ecda92eb2e88ae9f260d4a3
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2020-11-25 21:58:47 -0500

    Login Tab Created

--- src/pojo/User.java
+++ src/pojo/User.java
@@ -12,8 +12,8 @@ public class User {
 
     private int userId;
     private int userLoginId;
-    private static String firstName;
-    private static String lastName;
+    private String firstName;
+    private String lastName;
     private boolean isAdmin;
 
     private User(int loginId) {
@@ -44,32 +44,17 @@ public class User {
         return this.firstName + " " + lastName;
     }
 
-//    private void setUser(int loginId){
-//        User user1 = new User();
-//
-//        Database db = Database.getInstance();
-//
-//        String query = "SELECT * FROM user where user_login_id = " + loginId;
-//
-//        try{
-//            Statement statement = db.getConnection().createStatement();
-//            ResultSet resultSet = statement.executeQuery(query);
-//
-//            if(resultSet.next()){
-//                user1.userId = resultSet.getInt(DBConst.USER_COLUMN_ID);
-//                user1.userLoginId = resultSet.getInt(DBConst.USER_COLUMN_LOGIN_ID);
-//                user1.firstName = resultSet.getString(DBConst.USER_COLUMN_FIRST_NAME);
-//                user1.lastName = resultSet.getString(DBConst.USER_COLUMN_LAST_NAME);
-//                user1.isAdmin = resultSet.getBoolean(DBConst.USER_COLUMN_IS_ADMIN);
-//
-//                this.user = user1;
-//
-//            }
-//
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//        }
-//    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
 
     public static void setInstance(int userLoginId){
 
--- src/tabs/Login.java
+++ src/tabs/Login.java
@@ -1,12 +1,56 @@
 package tabs;
 
-import javafx.scene.control.Tab;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class Login extends Tab {
     private static Login tab;
 
     private Login() {
         super("Login");
+
+
+        VBox root = new VBox();
+        HBox hBox = new HBox();
+        Font font = Font.font("Times New Roman",30);
+
+        GridPane gridPane = new GridPane();
+        Label userNameLabel = new Label("User Name:");
+        userNameLabel.setFont(font);
+        Label passwordLabel = new Label("Password:");
+        passwordLabel.setFont(font);
+
+        TextField userName = new TextField();
+        userName.setFont(font);
+        PasswordField password = new PasswordField();
+        password.setFont(font);
+
+        gridPane.add(userNameLabel, 0 ,0);
+        gridPane.add(userName, 1,0);
+        gridPane.add(passwordLabel, 0,1);
+        gridPane.add(password, 1,1);
+
+        gridPane.setAlignment(Pos.BASELINE_RIGHT);
+        gridPane.setVgap(15);
+        gridPane.setHgap(5);
+
+        Button loginButton = new Button("Login");
+        loginButton.setFont(font);
+        gridPane.add(loginButton, 1,2);
+
+        hBox.getChildren().add(gridPane);
+        hBox.setAlignment(Pos.CENTER);
+
+        root.getChildren().addAll(hBox);
+        root.setAlignment(Pos.CENTER);
+
+        this.setContent(root);
+
     }
 
     public static Login getInstance(){
